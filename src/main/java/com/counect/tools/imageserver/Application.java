636d24212b4a75051c756be515570b4d7c1ec317
马岩 <yan.ma@counect.com>
2017-05-02 11:23:31 +0800

    finish

---
+++ src/main/java/com/counect/tools/imageserver/Application.java
@@ -0,0 +1,14 @@
+package com.counect.tools.imageserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by mayan on 17-4-27.
+ */
+@SpringBootApplication
+public class Application {
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class,args);
+  }
+}
---
+++ src/main/java/com/counect/tools/imageserver/Controller.java
@@ -0,0 +1,118 @@
+package com.counect.tools.imageserver;
+
+import com.aliyun.oss.ClientException;
+import com.aliyun.oss.OSSClient;
+import com.aliyun.oss.OSSException;
+import com.aliyun.oss.model.ObjectMetadata;
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import javax.activation.MimetypesFileTypeMap;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.lang.RandomStringUtils;
+import org.apache.commons.lang.time.DateUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ * Created by mayan on 17-4-27.
+ */
+@RestController
+public class Controller {
+
+  private static final String FILE_NAME_FORMAT = "%s-%s.%s";
+  private static final String[] SHOULD_FORMATTED_IMAGE_EXTENSIONS = {"jpeg", "jpg", "png"};
+  private static final Logger LOGGER = LoggerFactory.getLogger("image-server");
+
+  @Value("${image-server}")
+  private String IMAGE_SERVER;
+  @Value("${oss-endpoint}")
+  private String OSS_ENDPOINT;
+  @Value("${oss-access-key-id}")
+  private String OSS_ACCESS_KEY_ID;
+  @Value("${oss-access-key-secret}")
+  private String OSS_ACCESS_KEY_SECRET;
+  @Value("${oss-bucket-name}")
+  private String OSS_BUCKET_NAME;
+
+  private boolean uploadFileToOSS(File file, String filename) {
+    OSSClient client = new OSSClient(OSS_ENDPOINT, OSS_ACCESS_KEY_ID, OSS_ACCESS_KEY_SECRET);
+    try {
+      ObjectMetadata om = new ObjectMetadata();
+      om.setContentType(MimetypesFileTypeMap.getDefaultFileTypeMap().getContentType(file));
+      om.setLastModified(new Date());
+      client.putObject(OSS_BUCKET_NAME, filename, file, om);
+      return true;
+    } catch (ClientException e) {
+      LOGGER.error("OSS can not connected.", e);
+      return false;
+    } catch (OSSException e) {
+      LOGGER.error(String.format("Code:%s\nMessage:%s\nRequestId:%s\nHostId:%s", e.getErrorCode(),
+          e.getErrorMessage(), e.getRequestId(), e.getHostId()), e);
+      return false;
+    } finally {
+      client.shutdown();
+    }
+  }
+
+  @GetMapping
+  public String image(String filename) {
+    OSSClient client = new OSSClient(OSS_ENDPOINT, OSS_ACCESS_KEY_ID, OSS_ACCESS_KEY_SECRET);
+    String query = "";
+    try {
+      query = "?" + client
+          .generatePresignedUrl(OSS_BUCKET_NAME, filename, DateUtils.addHours(new Date(), 1))
+          .getQuery();
+    } catch (ClientException e) {
+      LOGGER.error("OSS can not connected.", e);
+    } catch (OSSException e) {
+      LOGGER.error(String.format("Code:%s\nMessage:%s\nRequestId:%s\nHostId:%s", e.getErrorCode(),
+          e.getErrorMessage(), e.getRequestId(), e.getHostId()), e);
+    } finally {
+      client.shutdown();
+    }
+    return "redirect:" + IMAGE_SERVER + filename + query;
+  }
+
+  @PostMapping
+  public void upload(MultipartFile file) throws IOException {
+    String filename = generateFilename(file);
+    File local = saveFileToLocal(file,filename);
+    if (FilenameUtils.isExtension(filename, SHOULD_FORMATTED_IMAGE_EXTENSIONS)) {
+      convertImage(local);
+    }
+    File formattedFile = new File(local.getAbsolutePath());
+    uploadFileToOSS(formattedFile, filename);
+    FileUtils.deleteQuietly(formattedFile);
+  }
+
+  private void convertImage(File local) throws IOException {
+    try {
+      Runtime.getRuntime().exec(String
+          .format("gm convert -strip -resize 1024x1024 %s %s", local.getAbsolutePath(),
+              local.getAbsolutePath())).waitFor();
+    } catch (InterruptedException e) {
+      LOGGER.error(e.getMessage(), e);//It should not happen.
+    }
+  }
+
+  private File saveFileToLocal(MultipartFile file,String filename) throws IOException {
+    File local = new File(filename);
+    FileUtils.copyInputStreamToFile(file.getInputStream(), local);
+    return local;
+  }
+
+  private String generateFilename(MultipartFile file) {
+    String dateStr = new SimpleDateFormat("yyyyMMddHHmmss").format(new Date());
+    String randomStr = RandomStringUtils.randomAlphanumeric(6);
+    String extensionStr = FilenameUtils.getExtension(file.getName()).toLowerCase();
+    return String.format(FILE_NAME_FORMAT, dateStr, randomStr, extensionStr);
+  }
+}
